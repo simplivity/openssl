e25a3276ecc3dec7ed3c0d04698a29214ee9a432
John Foley <John.Foley@simplivity.com>
2017-01-12 12:47:07 -0500

    OMNI-15795: Incremental commit, now supporting debug build and installing into build/Shared tree.

--- util/buildFOM.sh
+++ util/buildFOM.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 #
+# Usage:
+#   util/buildFom.sh [Shared|Static] [Debug|RelWithDebInfo]
+#
+
+echo "buildFOM..."
+echo ${1}
+echo ${2}
+
+if [ "${2}" = "Debug" ]; then
+	echo "Debug build requested"
+	DEBUG=" -d"
+fi
 
 cd fom
 tar -xzvf openssl-fips*.tar.gz
@@ -9,7 +21,7 @@ make
 make install
 cd ../..
 
-./config shared fips --with-fipsdir=/tmp/$$/fips no-idea no-mdc2 no-rc5 no-zlib enable-tlsext no-ssl2 enable-ec_nistp_64_gcc_128
+./config shared fips --with-fipsdir=/tmp/$$/fips no-idea no-mdc2 no-rc5 no-zlib enable-tlsext no-ssl2 enable-ec_nistp_64_gcc_128 --prefix=$PWD/build/${1}/${2} ${DEBUG}
 make depend
 make clean
 make
