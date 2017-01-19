2e59e1cc057b31f81971e510f4796e50b9565c5f
John Foley <John.Foley@simplivity.com>
2017-01-19 13:38:17 -0500

    OMNI-16514: Windows support to read desired FIPS state from svtarb.xml

--- crypto/o_init.c
+++ crypto/o_init.c
@@ -66,26 +66,27 @@
 #endif
 
 
-#define SVT_CFG_FILE "/var/svtfs/0/myconf/static/svtfs.xml"
+#ifdef OPENSSL_SYS_WIN32
+# define snprintf(buf, len, format, ...) _snprintf_s(buf, len, len, format, __VA_ARGS__)
+# define SVT_CFG_FILE "Simplivity\\Simplivity Arbiter\\var\\svtfs\\0\\myconf\\static\\svtarb.xml"
+#else
+# define SVT_CFG_FILE "/var/svtfs/0/myconf/static/svtfs.xml"
+#endif
 #define SVT_CFG_LINE_MAX 4096
 #define SVT_CFG_FIPS_KNOB "<FIPSMode>"
 
 static void init_fips_mode_from_svtcfg(void)
 {
-#ifdef OPENSSL_SYS_WIN32
-    /*
-     * For Windows, we always enable FIPS since the Arbiter is the only
-     * user of the FIPS module and we have no cfgdb.
-     */
-    if (!FIPS_mode_set(1))
-    {
-        CRYPTOerr(CRYPTO_F_FIPS_MODE_SET, CRYPTO_R_FIPS_MODE_NOT_SUPPORTED);
-        ERR_add_error_data(1, "Failed to enter FIPS mode.");
-    }
-#else
     char buf[SVT_CFG_LINE_MAX];
+    char cfg_file_nm[1024];
     FILE *cfg_file;
     int found_knob = 0;
+
+#ifdef OPENSSL_SYS_WIN32
+    snprintf(cfg_file_nm, 1024, "%s\\%s", getenv("ProgramFiles"), SVT_CFG_FILE);
+#else
+    snprintf(cfg_file_nm, 256, "%s", SVT_CFG_FILE);
+#endif
 	
     if (getenv("SVT_FORCE_FIPS_MODE") != NULL) 
     {
@@ -100,7 +101,7 @@ static void init_fips_mode_from_svtcfg(void)
         /* 
          * Open SVT config file and look for desired FIPS state
          */
-        cfg_file = fopen(SVT_CFG_FILE, "r"); 
+        cfg_file = fopen(cfg_file_nm, "r"); 
         if (cfg_file > 0) 
         {
             while (fgets(buf, SVT_CFG_LINE_MAX, cfg_file)) 
@@ -129,6 +130,7 @@ static void init_fips_mode_from_svtcfg(void)
         } 
         else 
         {	
+            fprintf(stderr,"Unable to determin desired FIPS mode: unable to open %s\n", cfg_file_nm);
             CRYPTOerr(CRYPTO_F_FIPS_MODE_SET, CRYPTO_R_FIPS_MODE_NOT_SUPPORTED);
             ERR_add_error_data(2, "OpenSSL unable open SVT configuration: ", strerror(errno));
         }   
@@ -138,7 +140,6 @@ static void init_fips_mode_from_svtcfg(void)
             ERR_add_error_data(1, "WARNING: FIPSMode setting not in SVT config.");
         }
     }
-#endif
 }
 
 /*
