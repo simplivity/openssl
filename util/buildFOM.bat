b91f307c7d604b66f4ff317aed753a1e5ffe135e
John Foley <john.foley@simplivity.com>
2017-01-20 14:40:59 -0500

    OMNI-16514: Use local tree for FOM install.

--- util/buildFOM.bat
+++ util/buildFOM.bat
@@ -2,6 +2,7 @@ set type=%1
 set mode=%2
 
 cd fom
+set FOMDIR=%cd%/fips-install
 
 if exist openssl-fips-* goto BUILDFOM
 gzip -d openssl-fips-*.tar.gz
@@ -9,7 +10,7 @@ tar -xvf openssl-fips-*.tar
 
 :BUILDFOM
 cd openssl-fips-*
-perl Configure VC-WIN64A no-asm
+perl Configure VC-WIN64A no-asm --prefix=%FOMDIR%
 call ms\do_win64a.bat
 nmake -f ms\ntdll.mak clean
 nmake -f ms\ntdll.mak
@@ -20,7 +21,7 @@ if "%type%" == "Static" set STATICOPT=no-shared
 if "%mode%" == "Debug" set DEBUGOPT=debug-
 
 :BUILDLIB
-perl Configure %DEBUGOPT%VC-WIN64A fips %STATICOPT% no-asm --prefix=build\%type%\%mode% 
+perl Configure %DEBUGOPT%VC-WIN64A fips %STATICOPT% no-asm --with-fipsdir=%FOMDIR% --prefix=build\%type%\%mode% 
 call ms\do_win64a.bat
 nmake -f ms\ntdll.mak clean
 nmake -f ms\ntdll.mak
