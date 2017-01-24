e35c98deab49d235c728410c98c7a1a317c3da9e
John Foley <John.Foley@simplivity.com>
2017-01-24 14:00:41 -0500

    Enable no-zlib option to match legacy OpenSSL Arbiter build.

--- util/buildFOM.bat
+++ util/buildFOM.bat
@@ -27,7 +27,7 @@ if "%type%" == "Static" set STATICOPT=no-shared
 if "%mode%" == "Debug" set DEBUGOPT=debug-
 
 :BUILDLIB
-perl Configure %DEBUGOPT%VC-WIN64A fips %STATICOPT% no-asm --with-fipsdir=%FOMDIR% --prefix=build\%type%\%mode% 
+perl Configure %DEBUGOPT%VC-WIN64A fips %STATICOPT% no-asm no-zlib --with-fipsdir=%FOMDIR% --prefix=build\%type%\%mode% 
 call ms\do_win64a.bat
 nmake -f ms\ntdll.mak clean
 nmake -f ms\ntdll.mak
