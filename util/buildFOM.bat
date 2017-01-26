4cc331f944974e7c4c7d868b5d7cdc476cb5078a
John Foley <John.Foley@simplivity.com>
2017-01-26 10:38:45 -0500

    Apply enginesdir patch and configure Linux build to use Ubuntu engine directory. Remove hard-coded path from Windows build script.

--- util/buildFOM.bat
+++ util/buildFOM.bat
@@ -4,8 +4,6 @@ set mode=%2
 cd fom
 set FOMDIR=%cd%\fips-install
 
-echo %NASM_DIR%
-set PATH=c:\temp\perl\bin;e:\Git\bin;%NASM_DIR%;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
 call nasm -v
 call perl -v
