12adbd00dd88ad4c482fa40a1aecffe064dc34e4
Alexander Volanis <alexander.volanis@simplivity.com>
2017-02-08 00:09:33 -0500

    OMNI-15320: Cleanup the Gradle build and configure new artifacts
    The build for Windows and Linux will generate the exact same set
    of artifacts with all available modes. We split the artifacts in
    three sets to manage the usage better. The main libraries including
    the fipscanister with the signature hash are packaged in the common
    NAR files to help with the builds. The Crypto engines are packaged
    in a separate archive because they are not commonly needed for our
    purposes. Finally the FIPS build tools including the fips_premain
    source and signature files are packaged in a third archive to use
    for the Windows Arbiter builds.

--- util/buildFOM.sh
+++ util/buildFOM.sh
@@ -1,32 +1,38 @@
 #!/bin/sh
 #
 # Usage:
-#   util/buildFom.sh [Shared|Static] [Debug|RelWithDebInfo]
+#   util/buildFom.sh [Debug|Release]
+#
+# Linux builds create both shared and static libraries from
+# a single execution so we select just Debug/Release options.
 #
 
 echo "buildFOM..."
 echo ${1}
-echo ${2}
+
+INSTALL_BASE=${BUILD_DIR:-${PWD}}
+FOMDIR=${INSTALL_BASE}/${1}/fips-install
+SSLDIR=${INSTALL_BASE}/${1}/openssl-install
 
 DEB_HOST_MULTIARCH=`dpkg-architecture -qDEB_HOST_MULTIARCH`
 
-if [ "${2}" = "Debug" ]; then
-	echo "Debug build requested"
-	DEBUG=" -d"
+if [ "${1}" = "Debug" ]; then
+    echo "Debug build requested"
+    DEBUG=" -d"
 fi
 
 cd fom
 tar -xzvf openssl-fips*.tar.gz
 cd openssl-fips*
-./config fipscanisteronly --prefix=/tmp/$$/fips
+./config fipscanisteronly --prefix=$FOMDIR
+make clean
 make
 make install
 cd ../..
 
-./config shared fips --with-fipsdir=/tmp/$$/fips no-idea no-mdc2 no-rc5 no-zlib enable-tlsext no-ssl2 enable-ec_nistp_64_gcc_128 --prefix=/usr --openssldir=/usr/lib/ssl --libdir=lib/${DEB_HOST_MULTIARCH} --enginesdir=/usr/lib/${DEB_HOST_MULTIARCH}/openssl-1.0.0/engines ${DEBUG}
+./config fips shared --with-fipsdir=$FOMDIR no-idea no-mdc2 no-rc5 no-zlib enable-tlsext no-ssl2 enable-ec_nistp_64_gcc_128 --prefix=/usr --openssldir=/usr/lib/ssl --libdir=lib/${DEB_HOST_MULTIARCH} --enginesdir=/usr/lib/${DEB_HOST_MULTIARCH}/openssl-1.0.0/engines ${DEBUG}
 make depend
 make clean
 make
 make test
-make INSTALL_PREFIX=$PWD/build/${1}/${2} install_sw
-
+make INSTALL_PREFIX=$SSLDIR install_sw
