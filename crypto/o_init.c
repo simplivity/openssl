c57c6701056790d5a6d561a076c4a6ed7b3432fa
Alex Volanis <alexander.volanis@simplivity.com>
2018-10-30 15:55:43 -0400

    Address review comments and tasks
    Separated the SVT specific init code into o_hpe_init.c and changed
    Makefile to build correctly.
    
    Fixed moduledependencies.yaml to reference the correct win3p version.
    
    Fixed indentation in libeay.num file.

--- crypto/o_init.c
+++ crypto/o_init.c
@@ -64,88 +64,9 @@
 void FIPS_crypto_set_id_callback(unsigned long (*func)(void));
 # endif
 
-# ifndef OPENSL_SYS_WIN32
-#  include <stdio.h>
-#  include <string.h>
-#  include <errno.h>
-# endif
+/* From o_hpe_init.c */
+void init_fips_mode_from_svtcfg(void);
 #endif
-
-
-#ifdef OPENSSL_SYS_WIN32
-# define snprintf(buf, len, format, ...) _snprintf_s(buf, len, len, format, __VA_ARGS__)
-# define SVT_CFG_FILE "Simplivity\\Simplivity Arbiter\\var\\svtfs\\0\\myconf\\static\\svtarb.xml"
-#else
-# define SVT_CFG_FILE "/var/svtfs/0/myconf/static/svtfs.xml"
-#endif
-#define SVT_CFG_LINE_MAX 4096
-#define SVT_CFG_FIPS_KNOB "<FIPSMode>"
-
-static void init_fips_mode_from_svtcfg(void)
-{
-    char buf[SVT_CFG_LINE_MAX];
-    char cfg_file_nm[1024];
-    FILE *cfg_file;
-    int found_knob = 0;
-
-#ifdef OPENSSL_SYS_WIN32
-    snprintf(cfg_file_nm, 1024, "%s\\%s", getenv("ProgramFiles"), SVT_CFG_FILE);
-#else
-    snprintf(cfg_file_nm, 256, "%s", SVT_CFG_FILE);
-#endif
-	
-    if (getenv("SVT_FORCE_FIPS_MODE") != NULL) 
-    {
-        if (!FIPS_mode_set(1))
-        {
-            CRYPTOerr(CRYPTO_F_INIT_FIPS_MODE_FROM_SVTCFG, CRYPTO_R_FIPS_MODE_NOT_SUPPORTED);
-            ERR_add_error_data(1, "Failed to enter FIPS mode.");
-        }
-    } 
-    else 
-    { 
-        /* 
-         * Open SVT config file and look for desired FIPS state
-         */
-        cfg_file = fopen(cfg_file_nm, "r"); 
-        if (cfg_file > 0) 
-        {
-            while (fgets(buf, SVT_CFG_LINE_MAX, cfg_file)) 
-            {
-                /*
-                 * Check if this line contains the FIPS setting
-                 */
-                if (strstr(buf, SVT_CFG_FIPS_KNOB)) 
-                {
-                    found_knob = 1;
-                    /*
-                     * See if FIPS should be enabled
-                     */
-                    if (strstr(buf, "true")) 
-                    {
-                        if (!FIPS_mode_set(1))
-                        {
-                            CRYPTOerr(CRYPTO_F_INIT_FIPS_MODE_FROM_SVTCFG, CRYPTO_R_FIPS_MODE_NOT_SUPPORTED);
-                            ERR_add_error_data(1, "Failed to enter FIPS mode.");
-                        }
-                        break;
-                    }
-                }
-            }
-            fclose(cfg_file);
-        } 
-        else 
-        {	
-            CRYPTOerr(CRYPTO_F_INIT_FIPS_MODE_FROM_SVTCFG, CRYPTO_R_FIPS_MODE_NOT_SUPPORTED);
-            ERR_add_error_data(4, "OpenSSL unable to open SVT configuration: ", cfg_file_nm, " Error: ", strerror(errno));
-        }   
-        if (!found_knob)
-        {
-            CRYPTOerr(CRYPTO_F_INIT_FIPS_MODE_FROM_SVTCFG, CRYPTO_R_FIPS_MODE_NOT_SUPPORTED);
-            ERR_add_error_data(1, "WARNING: FIPSMode setting not in SVT config.");
-        }
-    }
-}
 
 /*
  * Perform any essential OpenSSL initialization operations. Currently only
