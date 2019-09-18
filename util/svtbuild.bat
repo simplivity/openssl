a073b6f09656b6a4adab7d1970116c547b486da1
Alex Volanis <alexander.volanis@simplivity.com>
2019-09-18 05:26:26 +0000

    PMB-1108: Initial add of 18.04 OpenSSL 1.1.1d build
    (cherry picked from commit dd061e675892fe529801a997c3d029b3865ef299)
    
    PMB-1108: Fix debianDist to point to bionic
    
    (cherry picked from commit a552b21249d98d268114268c5d0f6a7be8885be0)
    
    PMB-1108: Add missing Jenkinsfile, and settings.gradle
    
    (cherry picked from commit 1673e1c67a787e59de59821af9792f6236fed26f)
    
    PMB-1108: Somehow a zero length JAR file was submitted for this
    
    (cherry picked from commit 99f0144cc7b16892cf4f6a377bc3797c76ef648e)
    
    PMB-1108: Packaging changes
    
    Change occurrences of 1.0.0 to 1.1.0
    Change engines occurrences to engines-1.1
    Remove fips tools
    
    (cherry picked from commit 7fc3426ea5be6a7698d403b29d41143bbecfaf86)
    
    PMB-1108: Remove svt-openssl-fips package, fix some debian packaging
    
    (cherry picked from commit 981c5d284c4d906ef2a8fba6c7e8afc772005334)
    
    PMB-1108: Fix package naming to be compatible with Ubuntu packaging
    
    (cherry picked from commit 6a85b3901d0eb8f05c1e0b64ad509371b0610b25)
    
    PMB-1108: Fix Windows builds to work with VS 2017
    
    (cherry picked from commit 992ebf74d9703ea97fc2f26221bddd773f66011f)
    
    PMB-1108: Use new plugins to setup VS 2010 environment correctly
    
    (cherry picked from commit b3c4b253e0d470f52276c22400844c69429c7829)
    
    PMB-1108: C++14 enablement
    
    (cherry picked from commit 5a644c488fe08eaafc337c15750b397ca6bc1a58)
    
    PMB-1108: VS 2017 build changes
    
    (cherry picked from commit 73588bb38827a15cd3499a01b2002603f6a19117)
    
    PMB-1108: Fix NAR packaging to include the PDB files
    
    (cherry picked from commit 5519bd1dbfa65ed1aa837dc31864f94b64641ff0)
    
    INFRA-5012: Disable stack protector for Release
    
    Disable the stack protector for release builds only.
    
    Debug builds get the default stack protector code.
    
    (cherry picked from commit d8725934675343d12e77957c1e6b393765565bab)

---
+++ util/svtbuild.bat
@@ -0,0 +1,43 @@
+setlocal
+
+REM
+REM We expect the caller has already setup the vcvarsall.bat compilation environment
+REM
+
+set COUNT=0
+for %%x in (%*) do set /A COUNT+=1
+
+if %COUNT% LSS 2 (
+    echo "ERROR: Expected 2 arguments, received %COUNT%" && exit /b 1
+)
+
+set type=%1
+set mode=%2
+
+if defined PROJECT_DIR (
+	set SOURCE_DIR=%PROJECT_DIR%
+) else (
+	set SOURCE_DIR=%CD%
+)
+
+if defined BUILD_DIR (
+    set FOMDIR=%BUILD_DIR%\%type%\%mode%\fips-install
+    set SSLDIR=%BUILD_DIR%\%type%\%mode%\openssl-install
+) else (
+    set FOMDIR=%CD%\%type%\%mode%\fips-install
+    set SSLDIR=%CD%\%type%\%mode%\openssl-install
+)
+
+if "%type%" == "Static" set SHARED=no-shared
+if "%mode%" == "Debug" set DEBUGOPT=--debug
+
+set PATH=%PERL_DIR%;%PATH%;%ProgramFiles%\Git\usr\bin
+
+call nasm -v
+call perl -v
+
+:BUILDLIB
+perl "%SOURCE_DIR%\Configure" VC-WIN64A %DEBUGOPT% %SHARED% no-dynamic-engine no-asm no-idea no-mdc2 no-rc5 no-zlib no-ssl3 no-ssl3-method enable-rfc3779 enable-cms --prefix=%SSLDIR% --openssldir=%SSLDIR%\ssl
+
+nmake
+nmake install_sw
